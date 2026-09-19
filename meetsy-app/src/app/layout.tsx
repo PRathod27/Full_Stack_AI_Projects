5d39fbfaada2ca141a5bfdb895f618b4b844a358
Prem Rathod <prem.rathod.2715@gmail.com>
2026-09-19 23:54:21 +0530

    Implemented clerk in the application

--- meetsy-app/src/app/layout.tsx
+++ meetsy-app/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const outfitFont = Outfit({
   subsets: ["latin"],
@@ -14,7 +21,24 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${outfitFont.className} antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ClerkProvider>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
--- meetsy-app/src/app/page.tsx
+++ meetsy-app/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { PricingTable } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Hello Next.Js</h1>
+      <PricingTable />
     </div>
   );
 }
